41f2a550a265a1d022d3e135c4b38f5d12abc05e
Taukon <83957880+Taukon@users.noreply.github.com>
2023-12-29 04:02:47 +0900

    fix

--- src/renderer/config.ts
+++ src/renderer/config.ts
@@ -1,1 +1,7 @@
 export const signalingAddress = `https://127.0.0.1:3000`;
+
+export const socketOption = {
+  secure: true,
+  rejectUnauthorized: false,
+  autoConnect: false, // necessary
+};
--- src/renderer/desktop/index.ts
+++ src/renderer/desktop/index.ts
@@ -3,7 +3,7 @@ import { listenAuth, reqAutoProxy } from "./signaling";
 import { ShareHostApp } from "./shareApp/host";
 import { ShareVirtualApp } from "./shareApp/virtual";
 import { ShareFile } from "./shareFile";
-import { signalingAddress } from "../config";
+import { signalingAddress, socketOption } from "../config";
 
 const setAuth = (desktopId: string, socket: Socket, password: string): void => {
   listenAuth(socket, desktopId, password);
@@ -153,11 +153,7 @@ export class Impromptu {
   private shareFile?: ShareFile;
 
   constructor() {
-    this.socket = io(signalingAddress, {
-      secure: true,
-      rejectUnauthorized: false,
-      autoConnect: false,
-    });
+    this.socket = io(signalingAddress, socketOption);
   }
 
   public listenDesktopId(
